6a0b914d52269e0149864c3d528da9a5a45a37bc
Celeste Herrera <celesteherrera281@gmail.com>
2021-03-11 17:38:53 -0600

    Primer commit v0.1

---
+++ RevStr2.js
@@ -0,0 +1,16 @@
+var palabra = "Cabdal"
+
+//console.log(palabra.length)
+//console.log(palabra)
+
+var palabraAlRevez = palabra.charAt(6) + palabra.charAt(5) + palabra.charAt(4) + palabra.charAt(3) + palabra.charAt(2) + palabra.charAt(1) + palabra.charAt(0)
+
+//console.log(imprimirPalabra)
+imprimirPalabra()
+function imprimirPalabra()
+{
+    console.log(palabra)
+    console.log(palabra.length)
+    console.log(palabraAlRevez)
+}
+
